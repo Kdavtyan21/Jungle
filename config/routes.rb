e26b8630d6c1eca478dedfca4c39d632ad7f0b31
Karen <karendavtyan21@mail.com>
2019-11-15 18:05:10 +0000

    added auntetication login adn register

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'products#index'
   
  
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create, :destroy]
+  end
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -12,12 +14,19 @@ Rails.application.routes.draw do
   end
 
   get "/about", to: "about#show"
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
